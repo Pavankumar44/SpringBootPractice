84ea6ad59bc5cdd9df16f8731bed84d2ce47374c
Pavankumar44 <pavan99424@gmail.com>
2021-05-08 11:54:14 +0530

    Update User.java

--- src/main/java/com/example/demo/bean/User.java
+++ src/main/java/com/example/demo/bean/User.java
@@ -3,6 +3,9 @@ package com.example.demo.bean;
 //import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
 //import javax.persistence.Table;
 
 @Entity
@@ -11,8 +14,12 @@ public class User {
 	
 	@Id
 	private Long id;
+	@NotEmpty(message = "The Username field can not be Empty!")
+	@Size(min=8,max=50, message="The Username Should beminimum of 8 Characters")
 	private String username;
+	@NotBlank(message="The Password field cannot be Empty")
 	private String password;
+	@NotEmpty(message="The address field cannot be Empty")
 	private String address;
 	private Long phone;
 	
